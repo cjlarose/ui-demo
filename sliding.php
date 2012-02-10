698db8102776812ea1a72e1258647233d40df2b2
Chris La Rose <cjlarose@iplantcollaborative.org>
2012-02-09 18:15:09 -0700

    Added real data to sliding.php

--- sliding.php
+++ sliding.php
@@ -1,51 +1,7 @@
 <?php
-$volume_data = array(
-	array('Volume 1', 'This volume does all kinds of things', '10'),
-	array('Cool Volume', 'This volume does super super cool things, I promise!', '12'),
-	array('Super super cool volume', 'This volume does cooler things than both volume 1 and volume 2 combined', '15'),
-	array('Volume 1', 'This volume does all kinds of things', '10'),
-	array('Cool Volume', 'This volume does super super cool things, I promise!', '12'),
-	array('Super super cool volume', 'This volume does cooler things than both volume 1 and volume 2 combined', '15'),
-	array('Volume 1', 'This volume does all kinds of things', '10'),
-	array('Cool Volume', 'This volume does super super cool things, I promise!', '12'),
-	array('Super super cool volume', 'This volume does cooler things than both volume 1 and volume 2 combined', '15')
-);
-$volumes = array();
-foreach ($volume_data as $volume_datum) {
-	$volumes[] = (object) array(
-		'name' => $volume_datum[0],
-		'description' => $volume_datum[1],
-		'size' => $volume_datum[2]
-	);	
-}
-
-$instance_data = array(
-	array('i-46190761', 'Image 1', 'emi-2F2B2523', 'New Base Image', '128.196.142.26', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-3A380612', 'Image 2', 'emi-2F2B2523', 'New Base Image', '128.196.142.68', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-46190763', 'Image 3', 'emi-2F2B2523', 'New Base Image', '128.196.142.29', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-46190764', 'Image 1', 'emi-2F2B2523', 'New Base Image', '128.196.142.26', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-3A380615', 'Image 2', 'emi-2F2B2523', 'New Base Image', '128.196.142.68', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-46190767', 'Image 3', 'emi-2F2B2523', 'New Base Image', '128.196.142.29', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-46190768', 'Image 1', 'emi-2F2B2523', 'New Base Image', '128.196.142.26', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-3A380619', 'Image 2', 'emi-2F2B2523', 'New Base Image', '128.196.142.68', 'primary', 'running', 1328630258, 'm1.small'),
-	array('i-4619076A', 'Image 3', 'emi-2F2B2523', 'New Base Image', '128.196.142.29', 'primary', 'running', 1328630258, 'm1.small')
-);
-
-$instances = array();
-foreach ($instance_data as $instance_datum) {
-	$instances[] = (object) array(
-		'name' => 'iPlant Base Image',
-		'id' => $instance_datum[0],
-		'description' => $instance_datum[1],
-		'image' => $instance_datum[2],
-		'image_name' => $instance_datum[3],
-		'address' => $instance_datum[4],
-		'key_name' => $instance_datum[5],
-		'state' => $instance_datum[6],
-		'launch_time' => $instance_datum[7],
-		'size' => $instance_datum[8]
-	);
-}
+include 'model.php';
+$instances_set = Instance::get_all_with_volumes();
+$instances = $instances_set->results;
 ?>
 <?php include 'header.php'; ?>
 <script type="text/javascript">
@@ -103,6 +59,10 @@ foreach ($instances as $instance) {
 					foreach ($instance as $key => $value) 
 						$meta_content .= "<dt>{$key}</dt><dd>{$value}</dd>";
 					$meta_content .= "</dl>";
+					
+					$attached_volumes_list_content = "";
+					foreach ($instance->Volume as $volume)
+						$attached_volumes_list_content .= "<li><h3>{$volume->name}</h3><p>{$volume->description}</p></li>";
 
 	echo "<div class=\"tabbable\">
   <ul class=\"nav nav-tabs\">
@@ -114,7 +74,7 @@ foreach ($instances as $instance) {
 {$meta_content}
     </div>
     <div class=\"tab-pane\" id=\"{$instance->id}-volumes\">
-		<ul class=\"attached-volumes-list\"></ul>	
+		<ul class=\"attached-volumes-list\">{$attached_volumes_list_content}</ul>	
     </div>
   </div>
 </div>		
